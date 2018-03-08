b4047b27bb6198340aaedbde815b02f119a24c3a
Waqas Dilawar <waqas.dilawar@moftak.com>
2018-03-08 21:08:57 +0500

    Added files

---
+++ LoRa/Aloha.r
@@ -0,0 +1,86 @@
+﻿timeSpan = 60 * 1000;
+#Mili Seconds
+timeInterval = 10;
+#Mili Seconds
+numberOfSlots = timeSpan / timeInterval;
+
+frequencySpan = 125e3;
+#Hertz :125 khz
+
+frequencyInterval = 100;
+# Hertez
+startChannel = 1;
+endChannel = 6;
+numberOfChannels = endChannel;
+fivePercent = 0;
+
+maxNumberOfDevices = 100;
+deviceStepSize = 1;
+
+numberOfDevices = deviceStepSize:deviceStepSize:maxNumberOfDevices;
+numberOfPackets = 1;
+spreadFactor <- c(12L, 11L, 10L, 09L, 08L, 07L, 06L);
+# For Passing to LoRa Matrix
+bitRate1 <- c(293L, 547L, 976L, 1757L, 3125L, 5478L, 9375L);
+#Bit Rate to check against Spreading Factor
+bitRate2 <- c(682L, 365L, 204L, 113L, 64L, 36L, 21L);
+#Bit Rate to check against Spreading Factor
+loraDuration <- cbind(spreadFactor, bitRate1, bitRate2);
+#row.names(loraDuration) <- c("SF With BitRate")
+packetDuration = loraDuration[, 3];
+#Packet Duration is according to BitRate2
+results <- matrix(0, maxNumberOfDevices / deviceStepSize, 2);
+for (noOfDevice in numberOfDevices) {
+    #ft = zeros(nrofslots, nrofchannels);
+    ft = matrix(0, numberOfSlots, numberOfChannels);
+    #ft2 = zeros(nrofslots, nrofchannels);
+    ft2 = matrix(0, numberOfSlots, numberOfChannels);
+    #colission = zeros(nr, nrofpackets);
+    colission = matrix(0, noOfDevice, numberOfPackets);
+    sf = randi(c(startChannel, endChannel), noOfDevice, numberOfPackets);
+    #time = randi([1 floor(nrofslots - (packetduration * numberOfPackets) / timeinterval)], [nr 1]);
+    for (i in 1:noOfDevice) {
+        timeOffset = floor((numberOfSlots - (packetDuration[sf[i] * numberOfPackets]) / timeInterval)
+                           * rand(1, 1));
+        for (p in 1:numberOfPackets) {
+            for (k in 0:0) {
+                if (sf[i,p] + k < 1 || (sf[i, p] + k > numberOfChannels))
+                #continue in MATLAB is similar to Next in R
+                    next;
+                duration = loraDuration[sf[i, p], 3];
+                flooredValue=floor(duration/timeInterval)
+                for (j in 1:flooredValue) {
+                    #freq(i, 1)
+                    if (j + timeOffset > numberOfSlots) {
+                        next;
+                    }
+                   currentIndex= ft[j + timeOffset, sf[i, p]];
+                    if (!is.null(currentIndex) && currentIndex == 0) {
+                        ft[j + timeOffset, sf[i, p] + k] = 1;
+                        ft2[j + timeOffset, sf[i, p] + k] = i;
+                    }
+                    else {
+                        ft[j + timeOffset, sf[i, p] + k] = ft[j + timeOffset, sf[i, p] + k] + 1;
+                        colission[i] = 1;
+                        colission[ft2[j + timeOffset, sf[i, p] + k]] = 1;
+                    }
+                }
+            }
+            timeOffset = ceil(timeOffset + duration / timeInterval);
+        }
+    }
+
+    results[floor(noOfDevice / deviceStepSize), 1] = sum(colission);
+    results[floor(noOfDevice / deviceStepSize), 2] = 100 * sum(colission) / noOfDevice;
+    #Row wise sum
+    fail = apply(colission, 1, sum) == numberOfPackets;
+
+    if (results[floor(noOfDevice / deviceStepSize), 2] < 5) {
+        fivePercent = noOfDevice;
+    }
+}
+sortedResult <- sort(results[,1])
+plot(numberOfDevices, results[, 1], main="Lora packet collision simulation withing 125 kH with \n %d devices
+transmitting randomly within 60 seconds", xlab="Number of 25 byte  messages  minute", ylab="Nr of collisions or Fails",
+type = "b", col = "red")
+
---
+++ LoRa/SlottedALOHA.r
@@ -0,0 +1,64 @@
+﻿sourceNumber <- 100;
+packetReadyProb <- 0.0057;
+maxBackoff <- 100;
+simulationTime <- 5000;
+
+sourceStatus <- matrix(0, 1, sourceNumber);
+
+#% legit source statuses are always non - negative integers and equal to:
+#% 0:source has no packet ready to be transmitted(is idle)
+#% 1:source has a packet ready to be transmitted, either because new data must be sent or a previously collided packet has waited the backoff time
+#% > 1:source is backlogged due to previous packets collision, the value of the status equals the number of slots it must wait for
+#the next transmission attempt
+sourceBackoff <- matrix(0, 1, sourceNumber);
+
+packtTransmissionAttempts = 0;
+ackdPacketDelay <- matrix(0, 1, simulationTime);
+
+ackdPacketCount = 0;
+packtCollisionCount = 0;
+packtGenerationTimestamp <- matrix(0, 1, sourceNumber);
+currentSlot = 0;
+while (currentSlot < simulationTime) {
+    currentSlot = currentSlot + 1;
+    for (eachSource1 in 1:length(sourceStatus)) {
+        #% new packet
+        if (sourceStatus[1, eachSource1] == 0 && rand(1) <= packetReadyProb) {
+            sourceStatus[1, eachSource1] = 1;
+            sourceBackoff[1, eachSource1] = randi(maxBackoff, 1);
+            packtGenerationTimestamp[1, eachSource1] = currentSlot;
+        }
+        else if (sourceStatus[1, eachSource1] == 1) {
+            #% backlogged packet
+            sourceBackoff[1, eachSource1] = randi(maxBackoff, 1);
+        }
+        packtTransmissionAttempts = packtTransmissionAttempts + sum(sourceStatus == 1);
+    }
+
+    if (sum(sourceStatus == 1) == 1) {
+        ackdPacketCount = ackdPacketCount + 1;
+        sourceId <- which(sourceStatus == 1, arr.ind = T)
+        ackdPacketDelay[ackdPacketCount] = currentSlot - packtGenerationTimestamp[sourceId];
+    } else if (sum(sourceStatus == 1) > 1) {
+        packtCollisionCount = packtCollisionCount + 1;
+        sourceStatus = sourceStatus + sourceBackoff;
+    }
+    sourceStatus = sourceStatus - 1;
+    #% decrease backoff interval
+    sourceStatus[sourceStatus < 0] = 0;
+    #% idle sources stay idle(see permitted statuses above)
+    sourceBackoff <- matrix(0, 1, sourceNumber);
+}
+trafficOffered = packtTransmissionAttempts / currentSlot;
+if (ackdPacketCount == 0) {
+    meanDelay = simulationTime;
+    #% theoretically, if packets collide continously, the delay tends to infinity
+} else {
+    meanDelay = mean(ackdPacketDelay[1,ackdPacketCount]);
+}
+throughput = ackdPacketCount / currentSlot;
+packtCollisionProb = packtCollisionCount / currentSlot;
+
+throughput <- throughput * 100;
+
+print(paste("ThroughPut is =", throughput,"%"));
